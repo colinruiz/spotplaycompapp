4f92c8ed4a7f48c7de7e81933124f833156d1732
ColinRuiz <colin.ruiz21@gmail.com>
2023-03-15 19:37:16 -0400

    Input boxes changed

--- comparison/forms.py
+++ comparison/forms.py
@@ -3,6 +3,8 @@ from .models import MyData
 
 class MyDataForm(forms.ModelForm):
     #playlist_id = forms.CharField(max_length=100)
+    playlist_id = forms.CharField(label = "Playlist ID")
+    
     class Meta:
         model = MyData
         fields = ['playlist_id']
