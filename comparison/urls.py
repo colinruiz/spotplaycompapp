38453fbfa9eb7252b74451fbc1ecae6892d36df4
EPalmer14 <zekepalmer14@gmail.com>
2023-03-23 12:54:11 -0400

    urls.py

--- comparison/urls.py
+++ comparison/urls.py
@@ -6,6 +6,4 @@ urlpatterns = [
     path('spotify/login/', spotify_login, name='spotify_login'),
     path('spotify/redirect/', spotify_callback, name='spotify_callback'),
     path('logout/', logout_view, name='logout_view'),
-    path('spotify/redirect/', dropdown_view1, name='dropdown_view1'),
-    path('spotify/redirect/', dropdown_view2, name='dropdown_view2')
 ]
