1eb9949042e733634c8c06ce52dbb8f5f0f3eef0
rkutyna <67471832+rkutyna@users.noreply.github.com>
2023-03-27 16:57:14 -0400

    added comparePlaylist to views

--- comparison/views.py
+++ comparison/views.py
@@ -5,6 +5,8 @@ from django.shortcuts import redirect
 from spotipy.oauth2 import SpotifyOAuth, CacheHandler
 from .forms import MyDataForm, SecondForm
 from django.http import HttpResponse
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials, SpotifyOAuth
 
 
 
@@ -82,9 +84,9 @@ def form(request):
         playlist_id2 = request.POST.get('playlist_id2')
         if (playlist_id2=="" or playlist_id1==""):
             return HttpResponse('fail')
-        print(playlist_id1, playlist_id2)
+        
         # Do something with the user_text
-        return HttpResponse('Success')
+        return HttpResponse(comparePlaylists(playlist_id1, playlist_id2))
     return render(request, 'success.html')
     # if request.method == 'POST':
     #     form = MyDataForm(request.POST)
@@ -132,3 +134,38 @@ def logout_view(request):
             return render(request, 'home.html')
     return render(request, 'home.html')
 
+
+def comparePlaylists(playlist1_id, playlist2_id):
+    
+    # scopes for auth_manager
+    scope = "user-read-playback-state app-remote-control streaming user-library-read"
+
+    #sp = spotipy.Spotify(auth_manager)
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(CLIENT_ID, CLIENT_SECRET, scope=scope, redirect_uri=REDIRECT_URI))
+    #sp = spotipy.Spotify(auth_manager=SpotifyOAuth('f8a3f82ba99d46a694d89bc1cdc1cb09', '0dcbdb4f9fd0496683a16c01c93a9377', scope=scope, redirect_uri="http://127.0.0.1:8000/spotify/redirect"))
+
+    # counts the number of shared tracks
+    count=0
+    # gets playlist lengths
+    length1=sp.playlist(playlist1_id)['tracks']['total']
+    length2=sp.playlist(playlist2_id)['tracks']['total']
+    # iterates through playlists and adds to a list to be compared
+    list2 = []
+    for k in range((length2//100)+1):
+        playlist2=sp.playlist_tracks(playlist2_id, limit=100, offset=100*k)['items']
+        for a in playlist2:
+            list2.append(a['track']['id'])
+                
+    list1 = []
+    for i in range((length1//100)+1):
+        playlist1=sp.playlist_tracks(playlist1_id, limit=100, offset=100*i)['items']
+        for a in playlist1:
+            list1.append(a['track']['id'])
+
+    # finds the number of common tracks
+    for i in list1:
+        #print(i)
+        if i in list2:
+            count+=1
+        
+    return "The two playlists have "+str(round((count/(length1+length2-count))*100, 2))+"%"+' in common'
