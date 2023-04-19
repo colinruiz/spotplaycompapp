62a44e0c65dafabcdffdb61669a493a35e4a3d69
ColinRuiz <colin.ruiz21@gmail.com>
2023-04-19 12:40:38 -0400

    Trying to make it public

--- comparison/credentials.py
+++ comparison/credentials.py
@@ -1,4 +1,4 @@
-CLIENT_ID = "0eb27e7c8598493fba46f54e10550e4f"
-CLIENT_SECRET = "c520c87edc224b069f8ef996a5287642"
+CLIENT_ID = "f5c7d77ddd6643cda3e454a4232589ea"
+CLIENT_SECRET = "e0686caefa43499b8c33dddc81a6efe1"
 REDIRECT_URI = "http://127.0.0.1:8000/spotify/redirect"
 SCOPES= "user-read-playback-state app-remote-control streaming user-library-read playlist-read-private playlist-read-collaborative"
--- comparison/views.py
+++ comparison/views.py
@@ -10,12 +10,9 @@ from .forms import DropdownForm
 import os
 from django.views.decorators.cache import never_cache
 from django.http import JsonResponse
+from .credentials import *
 
 
-CLIENT_ID = "0eb27e7c8598493fba46f54e10550e4f"
-CLIENT_SECRET = "c520c87edc224b069f8ef996a5287642"
-REDIRECT_URI = "http://127.0.0.1:8000/spotify/redirect"
-SCOPES= "user-read-playback-state app-remote-control streaming user-library-read playlist-read-private playlist-read-collaborative"
 
 
 def home(request):
